452bf83b0f6e4c94dacce4661089d7f476cfc404
cloviswrodrigues <cloviswrodrigues@gmail.com>
2022-12-11 23:44:00 -0300

    created js logic selected option

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -2,6 +2,8 @@ const btnBookmark = document.querySelector('.js-btn-bookmark');
 const btnSucess = document.querySelector('.js-btn-sucess');
 const modalClose = document.querySelector('.js-modal-close');
 const buttonsPledge = document.querySelectorAll('.js-btn-pledge');
+const inputsRadio = document.querySelectorAll('input[name="plan-pledge"]');
+const inputWithForm = ['pledge-option-2','pledge-option-3'];
 
 function btnMarked(e) {
     btnBookmark.classList.toggle('btn-bookmark--marked');
@@ -37,6 +39,37 @@ function closeModal() {
     overflowHiddenDisabled('body');
 }
 
+
+function activeForm(e) {
+    inputId = e.target.id;
+    if (inputWithForm.includes(inputId)){
+        disabledAllOptionsActive()
+
+        pledgeWrapper = e.target.parentNode.parentNode.parentNode;
+        form = pledgeWrapper.lastElementChild;
+
+        pledgeWrapper.classList.add('active-option');
+        form.classList.add('active-form');
+    }
+}
+
+
+function disabledAllOptionsActive() {
+    let options = document.querySelectorAll('.active-option');
+    let forms = document.querySelectorAll('.active-form');
+
+    options.forEach((e) => {
+        option = e;
+        option.classList.remove('active-option');
+    })
+
+    forms.forEach((e) => {
+        form = e;
+        form.classList.remove('active-form');
+    })
+}
+
+
 btnBookmark.addEventListener('click', btnMarked)
 btnSucess.addEventListener('click', closeModal)
 modalClose.addEventListener('click', closeModal)
@@ -44,3 +77,10 @@ modalClose.addEventListener('click', closeModal)
 buttonsPledge.forEach((e) =>
     e.addEventListener('click', openModalPledge)
 )
+
+inputsRadio.forEach((e) =>
+    e.addEventListener('click', activeForm)
+)
+
+
+console.log(inputsRadio)
